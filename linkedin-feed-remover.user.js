45b602698d72a1915deb45cf23d898794c47a50c
luizsaurin <bladerunner9040@gmail.com>
2026-06-25 08:24:09 -0300

    feat: add update and download urls

--- linkedin-feed-remover.user.js
+++ linkedin-feed-remover.user.js
@@ -2,12 +2,14 @@
 // @name         LinkedIn Feed Remover
 // @namespace    https://github.com/luizsaurin
 // @version      0.0.1
-// @description  Hide LinkedIn's social feed and use LinkedIn only for jobs, messaging, networking, and profile management
+// @description  Hide LinkedIn's annoying social feed
 // @author       luizsaurin
 // @homepageURL  https://github.com/luizsaurin/linkedin-feed-remover
 // @supportURL   https://github.com/luizsaurin/linkedin-feed-remover/issues
 // @match        https://www.linkedin.com/feed/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=linkedin.com
+// @updateUrl    http://raw.githubusercontent.com/luizsaurin/linkedin-feed-remover/main/linkedin-feed-remover.user.js
+// @downloadUrl  https://raw.githubusercontent.com/luizsaurin/linkedin-feed-remover/main/linkedin-feed-remover.user.js
 // @grant        none
 
 // ==/UserScript==
